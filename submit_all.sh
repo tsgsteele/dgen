03f3ca284f10892c03d6fee6bd38410e44b8ca55
Talor Gruenwald <talor.gruenwald@gmail.com>
2025-07-29 11:13:37 -0400

    Changing batch yaml, outputting new state csvs

--- submit_all.sh
+++ submit_all.sh
@@ -11,7 +11,7 @@ JOB_NAME="dgen-small-states-${JOB_TS}"
 gcloud batch jobs submit "${JOB_NAME}" \
   --location="${LOCATION}" \
   --config="dgen-batch-job-small-states.yaml" \
-  --machine-type="c2-standard-8" \
+  --machine-type="c2d-highcpu-16" \
   --provisioning-model="SPOT"
 
 # Define job name with timestamp
@@ -21,7 +21,7 @@ JOB_NAME="dgen-mid-states-${JOB_TS}"
 gcloud batch jobs submit "${JOB_NAME}" \
   --location="${LOCATION}" \
   --config="dgen-batch-job-mid-states.yaml" \
-  --machine-type="c2-standard-16"  \
+  --machine-type="c2d-highcpu-16"  \
   --provisioning-model="SPOT"
 
 # Define job name with timestamp
@@ -31,5 +31,5 @@ JOB_NAME="dgen-large-states-${JOB_TS}"
 gcloud batch jobs submit "${JOB_NAME}" \
   --location="${LOCATION}" \
   --config="dgen-batch-job-large-states.yaml" \
-  --machine-type="c2-standard-16"  \
+  --machine-type="c2d-highcpu-32"  \
   --provisioning-model="SPOT"
